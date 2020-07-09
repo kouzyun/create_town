126e48a5185712a9d169cb0f8f2992ae18ec3622
vagrant <vagrant@localhost.localdomain>
2020-07-09 06:14:16 +0000

    [Add]コントローラ、モデル、ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #デバイス
+  devise_for :users, skip: :all
+  devise_scope :users do
+    get '/users/sign_in', to: 'users/sessions#new'
+    post '/users/sign_in', to: 'users/sessions#create'
+    delete '/users/sign_out', to: 'users/sessions#destroy'
+    get '/users/sign_up', to: 'users/registrations#new'
+    post '/users', to: 'users/registrations#create'
+  end
+
+  #ルート設定
+  root "homes#top"
+
+  #aboutページ
+  get 'about' => 'homes#about'
+
+  #ユーザー
+  resources :users do
+    resources :relationships, only: [:create, :destroy] #フォロー
+    get 'follows' => 'relationships#follower', as: 'follows'
+    get 'followers' => 'relationships#followed', as: 'followers'
+  end
+
+  resources :posts do
+    resources :comments, only: [:create, :destroy] #コメント
+    resources :favorites, only: [:create, :destroy] #いいね
+  end
+
 end
