4840d46371d1f48ed284d0ae63b1fed29c4d6fd5
vagrant <vagrant@localhost.localdomain>
2020-07-10 12:16:10 +0000

    [Add]新規投稿画面を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
@@ -11,6 +13,7 @@ class PostsController < ApplicationController
 
   def create
   	post = Post.new(post_params)
+    post.user_id = current_user.id
   	post.save
   	redirect_to posts_path
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+	belongs_to :user
 	attachment :image
 
 	enum category: { 参加者募集中: 0, 考え事: 1, イベント: 2 },_suffix: true
