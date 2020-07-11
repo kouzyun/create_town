d389c7b221109fb5992a2c7df7e30257f2c51824
vagrant <vagrant@localhost.localdomain>
2020-07-11 07:55:05 +0000

    [fix]postテーブルのimage_idのデータ型を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_022051) do
+ActiveRecord::Schema.define(version: 2020_07_11_074612) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2020_07_11_022051) do
     t.integer "user_id"
     t.string "title"
     t.text "body"
-    t.integer "image_id"
+    t.string "image_id"
     t.integer "category"
     t.string "postal_code"
     t.string "address"
