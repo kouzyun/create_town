f9688adb6b67abd9526208a804eb02b8389340ec
vagrant <vagrant@localhost.localdomain>
2020-07-11 09:14:11 +0000

    [Add]投稿一覧ページ作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
 
 	enum category: { 参加者募集中: 0, 考え事: 1, イベント: 2 },_suffix: true
 
-	#geocoderがaddressカラムからlatitude,longitudeにも自動的に反映させる
+	#geocoderがaddressカラムからlatitude,longitudeに自動的に反映させる
 	geocoded_by :address
 	after_validation :geocode
 end
