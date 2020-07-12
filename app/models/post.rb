597e22a1fc6a0528da776397ea28313628295f02
vagrant <vagrant@localhost.localdomain>
2020-07-12 03:48:12 +0000

    [Add]投稿詳細ページを作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 	belongs_to :user
 	attachment :image
 
-	enum category: { 参加者募集中: 0, 考え事: 1, イベント: 2 },_suffix: true
+	enum category: { 参加者募集中！: 0, 考え事: 1, イベント開催: 2 },_suffix: true
 
 	#geocoderがaddressカラムからlatitude,longitudeに自動的に反映させる
 	geocoded_by :address
