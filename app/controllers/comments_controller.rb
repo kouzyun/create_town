03f3968aaec9e2704d5c9f61ed94e8184ed99fb3
kojihara <harakoji.0514@gmail.com>
2020-07-16 01:24:18 +0000

    [Add]commentsコントローラ：createメソッドを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,19 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!
+
+	def create
+		post = Post.find(params[:post_id])
+		comment = Comment.new(comment_params)
+		comment.user_id = current_user.id
+		comment.post_id = post.id
+		comment.save
+		redirect_to post_path(post)
+	end
+
+	def destroy
+	end
+
+	
+
 end
+
