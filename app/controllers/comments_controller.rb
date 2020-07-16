c22d2fc67abe3bc5c4c9ebee31424e195ef6389a
kojihara <harakoji.0514@gmail.com>
2020-07-16 02:16:19 +0000

    [Add]commentsコントローラ：destroyメソッドを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,18 +2,26 @@ class CommentsController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-		post = Post.find(params[:post_id])
-		comment = Comment.new(comment_params)
-		comment.user_id = current_user.id
-		comment.post_id = post.id
-		comment.save
+		@post = Post.find(params[:post_id])
+		@comment = Comment.new(comment_params)
+		@comment.user_id = current_user.id
+		@comment.post_id = @post.id
+		@comment.save
 		redirect_to post_path(post)
 	end
 
 	def destroy
+		@post = Post.find(params[:post_id])
+		comment = @post.comments.find(params[:id])
+		comment.destroy
+    	redirect_to post_path(params[:post_id])
 	end
 
-	
+	private
+
+	def comment_params
+	    params.require(:comment).permit(:content)
+	end
 
 end
 
