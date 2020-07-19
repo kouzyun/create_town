d60e73567decbeb7677d0b7cd4b1de50443e0b7a
kojihara <harakoji.0514@gmail.com>
2020-07-19 06:27:37 +0000

    [Add]いいね機能のdestroyメソッドを非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,7 +9,7 @@ class FavoritesController < ApplicationController
 
 	def destroy
 		@post = Post.find(params[:post_id])
-		favorite = current_user.favorites.find_by(post_id: @post.id)
+		favorite = current_user.favorites.find_by(post_id: @post.id, user_id: current_user.id)
 		favorite.destroy
 	end
 end
